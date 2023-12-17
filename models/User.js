9f2cf27f44d1e8ce33950c9c7d113925b07cfaa4
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-12-18 09:21:31 +1100

    user schema defined

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true, // Mongodb will only index docs where googleId exists
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  name: {
+    type: String,
+  },
+  avatarUrl: {
+    type: String,
+  },
+  password: {
+    type: String,
+    select: false,  // by default will not be returned from queries.
+  },
+  accountSettings: {
+    isPrivate: {
+      type: Boolean,
+      default: false
+    },
+    allowNonFriendMessages: {
+      type: Boolean,
+      default: true,
+    }
+  },
+}, {timestamps: true});
+
+UserSchema.plugin(require('mongoose-bcrypt'));
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
