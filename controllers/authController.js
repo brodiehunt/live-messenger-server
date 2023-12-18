46041d127abc7b47d7231b0afcab92744908612e
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-12-18 11:40:39 +1100

    Sign in local strategy, form validation, route handlers complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,29 @@
+const {signinValidation, handleValidation } = require('../utils/formValidations');
+const { generateToken } = require('../utils/generateJwt');
 
+exports.signin = [
+  signinValidation,
+  handleValidation,
+  async (req, res, next) => {
+    
+    const jwt = generateToken(req.user._id);
 
-exports.signin = async (req, res, next) => {
-  res.send('hello sign in');
-};
+    res.cookie('jwt', jwt, {
+      httpOnly: true, 
+    });
+
+    res.status(200).json({
+      message: 'Successful login',
+      data: req.user
+    });
+  }
+];
+
+exports.register = [
+  registerValidation,
+  handleValidation,
+  async (req, res, next) => {
+
+  }
+];
 
---
+++ utils/formValidations.js
@@ -0,0 +1,57 @@
+const {body, validationResult} = require('express-validator');
+
+exports.signinValidation = [
+  body('email')
+    .trim()
+    .toLowerCase()
+    .isEmail()
+    .withMessage('Invalid email'),
+  body('password')
+    .trim()
+    .isLength({min: 6, max: 20})
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+    .withMessage('Invalid password')
+];
+
+exports.registerValidation = [
+  body('name')
+    .trim()
+    .isLength({min: 3, max: 20})
+    .withMessage('Name must be more than 2 and less than 20 characters'),
+  body('username')
+    .trim()
+    .toLowerCase()
+    .isLength({min: 5, max: 15})
+    .withMessage('Invalid Username'),
+  body('email')
+    .trim()
+    .toLowerCase()
+    .isEmail()
+    .withMessage('Invalid email'),
+  body('password')
+    .trim()
+    .isLength({min: 6, max: 20})
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+    .withMessage('Invalid password')
+]
+
+
+
+exports.handleValidation = (req, res, next) => {
+  const result = validationResult(req);
+
+  if (result.isEmpty()) {
+    next();
+  }
+  // result errArr = [[path, msg], [path, msg]]
+  const errorsArr = result.errors.map((fieldObj) => {
+    return [fieldObj.path, fieldObj.msg]
+  })
+
+  // result errorsObj = {path: msg, path: msg}
+  const errorsObj = Object.fromEntries(errorsArr);
+
+  return res.status(400).json({
+    error: errorsObj
+  })
+}
